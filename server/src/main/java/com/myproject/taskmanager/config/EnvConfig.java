4e6c0e1ed49ce3690acfcd9e8a14bd464f8b52f1
unknown <noyco25@gmail.com>
2025-06-04 22:44:32 +0300

    הודעת commit שלך

--- server/src/main/java/com/myproject/taskmanager/config/EnvConfig.java
+++ server/src/main/java/com/myproject/taskmanager/config/EnvConfig.java
@@ -1,4 +1,27 @@
 package com.myproject.taskmanager.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
 public class EnvConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        Dotenv dotenv = Dotenv.load();
+
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl("jdbc:mysql://" +
+                dotenv.get("DB_HOST") + ":" +
+                dotenv.get("DB_PORT") + "/" +
+                dotenv.get("DB_NAME") + "?createDatabaseIfNotExist=true");
+        dataSource.setUsername(dotenv.get("DB_USERNAME"));
+        dataSource.setPassword(dotenv.get("DB_PASSWORD"));
+
+        return dataSource;
+    }
 }
--- server/src/test/java/com/myproject/taskmanager/mapper/TaskMapper.java
+++ server/src/test/java/com/myproject/taskmanager/mapper/TaskMapper.java
@@ -2,6 +2,7 @@ package com.myproject.taskmanager.mapper;
 
 import com.myproject.taskmanager.dto.TaskDto;
 import com.myproject.taskmanager.entity.Task;
+import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface TaskMapper {
